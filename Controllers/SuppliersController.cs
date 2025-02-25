365c16be1917c2575e43c932ffb381f81a68d09b
Nevena Milenković <nevena11milenkovic@gmail.com>
2025-02-26 00:28:45 +0100

    Add project files.

---
+++ Controllers/SuppliersController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tsql3s2a.Data;
+using Tsql3s2a.Models;
+using Tsql3s2a.Models.ViewModels;
+
+namespace Tsql3s2a.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly TsqlContext _context;
+
+        public SuppliersController(TsqlContext context)
+        {
+            _context = context;
+        }
+
+        //LISTA SUPPLIERA
+        public async Task<IActionResult> Index()
+        {
+            var s = await _context.Suppliers.ToListAsync();
+            return View(s);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Supplierid", "Companyname", "Contactname", "Contacttitle", "Address", "City", "Region", "Postalcode", "Country", "Phone", "Fax")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Suppliers.Add(supplier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var s = await _context.Suppliers.FirstOrDefaultAsync(s => s.Supplierid == id);
+            return View(s);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Supplierid", "Companyname", "Contactname", "Contacttitle", "Address", "City", "Region", "Postalcode", "Country", "Phone", "Fax")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(supplier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(supplier);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var s = await _context.Suppliers
+                .Include(s => s.Products)
+                .FirstOrDefaultAsync(s => s.Supplierid == id);
+
+            if (s == null)
+            {
+                return NotFound();
+            }
+
+            var model = new SupplierViewModel
+            {
+                Supplier = s,
+                Products = s.Products.ToList()
+            };
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var s = await _context.Suppliers
+                .Include(s => s.Products)
+                .FirstOrDefaultAsync(s => s.Supplierid == id);
+
+            if (s != null)
+            {
+                _context.Suppliers.Remove(s);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Models/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Tsql3s2a.Models.ViewModels
+{
+    public class SupplierViewModel
+    {
+        public Supplier Supplier { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
